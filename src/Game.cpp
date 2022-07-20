e2509449f3eed43e76d0a31855800fce7202469b
wkdrbcjs <101453735+wkdrbcjs@users.noreply.github.com>
2022-07-20 15:01:36 +0900

    6번
    5번에서 함수들을 Game.cpp에 써야 했는데 안했음,
    6번 단계에서 진행

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,9 +1,119 @@
+
 #include "Game.h"
-#include "main.h"
+
+// main에서 Init을 함수로 빼냄
+bool Game::init(const char* title, int xpos, int ypos,
+	int height, int width, int flags)
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	{
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+		if (m_pWindow != 0)
+		{
+			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
+
+			if (m_pRenderer != 0)
+			{
+				SDL_SetRenderDrawColor(m_pRenderer, 0, 0, 0, 255);
+			}
+			else
+			{
+				return false; // 랜더러 생성 실패
+			}
+		}
+		else 
+		{
+			return false; // 윈도우 생설 실패 l
+		}
+	}
+	else 
+	{
+		return false; // SDL 초기화 실패
+	}
+#pragma region a
+#pragma endregion
 
 
-SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+#pragma region Texture 생성
+	//  로드 파일을 RAM에 로드
+	SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
+	
+	//SDL_CreateTextureFromSurface 는 pTempSurface에 로드된 파일을 PU에 로드
+	m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+	SDL_FreeSurface(pTempSurface);
+#pragma endregion
+	
+#pragma region 원본상자 크기 초기화
+	SDL_QueryTexture(m_pTexture, NULL, NULL,
+		&m_sourceRectangle.w, &m_sourceRectangle.h);
+#pragma endregion
+	
+#pragma region 대상상자 크기 초기화
+	// 원본 상자의 크기를 대상 상자와 같은 크기가 되도록 설정
+	m_destinationRectangle.w = m_sourceRectangle.w;
+	m_destinationRectangle.h = m_sourceRectangle.h;
+#pragma endregion
 
-m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
 
-SDL_FreeSurface(pTempSurface);
+#pragma region 원본/대상 상자의 위치 설정
+	// 원본과 대상의 위치 또한 같도록
+	m_destinationRectangle.x = m_sourceRectangle.x = 0;
+	m_destinationRectangle.y = m_sourceRectangle.y = 0;
+#pragma endregion
+
+
+	m_bRunning = true;
+	return true;
+}
+// 키입력
+void Game::getCommand()
+{
+	if (_kbhit())
+	{
+		if (_getch() == 27)
+		{
+			m_bRunning = false;
+		}
+	}
+}
+
+void Game::update()
+{
+}
+
+// Init에서 설정된g_pRenderer 값으로 화면을 그려줄 것임.
+void Game::render()
+{
+	SDL_RenderClear(m_pRenderer);
+	SDL_RenderPresent(m_pRenderer);
+
+	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+}
+
+bool Game::running()
+{
+	return m_bRunning;
+}
+
+void Game::handleEvents()
+{
+	SDL_Event event;
+	if (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+			case SDL_QUIT:
+				m_bRunning = false;
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+void Game::clean()
+{
+	SDL_DestroyWindow(m_pWindow);
+	SDL_DestroyRenderer(m_pRenderer);
+	SDL_Quit();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -206,96 +206,18 @@ void render()
 	SDL_RenderPresent(g_pRenderer);
 }*/
 
-//#include <SDL.h> -> main.h 안에 <SDL2/SDL.h> 로 참조되어 있음
-#include "main.h"
-#include <conio.h>
 #include "Game.h"
 
 Game* g_game = 0;
-
-// main에서 Init을 함수로 빼냄
-bool Game::init(const char* title, int xpos, int ypos,
-	int height, int width, int flags)
-{
-	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
-		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-		if (m_pWindow != 0) {
-			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
-
-			if (m_pRenderer != 0) {
-				SDL_SetRenderDrawColor(m_pRenderer, 255, 255, 255, 255);
-			}
-			else {
-				return false; // 랜더러 생성 실패
-			}
-		}
-		else {
-			return false; // 윈도우 생설 실패 l
-		}
-	}
-	else {
-		return false; // SDL 초기화 실패
-	}
-
-	m_bRunning = true;
-	return true;
-}
-// 키입력
-void Game::getCommand()
-{
-	if (_kbhit())
-	{
-		if (_getch() == 27)
-		{
-			m_bRunning = false;
-		}
-	}
-}
-
-void Game::update()
-{
-}
-
-// Init에서 설정된g_pRenderer 값으로 화면을 그려줄 것임.
-void Game::render()
-{
-	SDL_RenderClear(m_pRenderer);
-	SDL_RenderPresent(m_pRenderer);
-}
-
-bool Game::running()
-{
-	return m_bRunning;
-}
-
-void Game::handleEvents()
-{
-	SDL_Event event;
-	if (SDL_PollEvent(&event)) 
-	{
-		switch (event.type) 
-		{
-			case SDL_QUIT:
-				m_bRunning = false;
-				break;
-			default:
-				break;
-		}
-	}
-}
-
-void Game::clean()
-{
-	SDL_DestroyWindow(m_pWindow);
-	SDL_DestroyRenderer(m_pRenderer);
-	SDL_Quit();
-}
 
 #pragma region GameClassCreate
 int main(int argc, char* args[])
 {
 	g_game = new Game();
-	g_game->init("GameClass", 100, 100, 640, 480, 0);
+	g_game->init("GameClass", 
+		SDL_WINDOWPOS_CENTERED, 
+		SDL_WINDOWPOS_CENTERED, 
+		480, 640, 0);
 	while (g_game->running())
 	{
 		g_game->getCommand();
